d3b8e07d82531d6ab1661f516d54eb6d3f30397e
Gabe Cziprusz <gcziprusz@gmail.com>
2025-10-27 09:16:51 -0500

    update vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
-import path from 'path'
+import path from 'node:path'
 import { normalizePath } from 'vite'
 import { dirname } from 'path'
 import { glob } from 'glob'
@@ -11,9 +11,6 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-import { normalizePath } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import path from 'node:path'
 
 export default defineConfig(({ mode }) => {
   // Load env variables based on mode for server access
